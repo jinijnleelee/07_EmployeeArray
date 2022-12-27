26db46a33a888eef53c1972c4cf3983f711d459f
LEE JINYOUNG <jinijnleelee@gmail.com>
2022-12-27 15:19:22 +0900

    객체배열 연습

---
+++ src/edu/kh/objarray/practice/model/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package edu.kh.objarray.practice.model.service;
+
+import edu.kh.objarray.practice.model.vo.Employee;
+
+public class EmployeeService {
+	
+	//private Employee
+
+}
---
+++ src/edu/kh/objarray/practice/model/vo/Employee.java
@@ -0,0 +1,9 @@
+package edu.kh.objarray.practice.model.vo;
+
+public class Employee {
+	private int empNo;
+	private String name;
+	
+	//String 
+
+}
